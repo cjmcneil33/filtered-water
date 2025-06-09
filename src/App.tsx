2556c1e051d601e0d82583564ddcb84442bde61c
Cole McNeil <ceej@MacBook-Air.local>
2025-06-09 15:03:13 -0600

    Getting the filtering to work and adding Reactbits

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import "./App.css";
 import "./Images.css";
 import "./Button.css";
 import { useState } from "react";
-import AnimatedComponent from "./Animation";
+// import AnimatedComponent from "./Animation";
 import Filter from "./Filter";
+import TextPressure from "./ReactbitsTextPressure";
 
 function App() {
   const [water, setWater] = useState<string[]>([
@@ -19,12 +20,27 @@ function App() {
   const handleOnClick = () => {
     let filtered: string[] = Filter(water, contaminants);
     setWater(filtered);
+    return filtered;
   };
 
   return (
     <>
       <section className="format">
-        <h2>Filter me!!!</h2>
+        <section style={{ position: "relative", height: "100px" }}>
+          <TextPressure
+            text="Filter me!!!"
+            flex={true}
+            alpha={false}
+            stroke={false}
+            width={true}
+            weight={true}
+            italic={true}
+            textColor="-webkit-linear-gradient(#4f543d, #0ac4c4)"
+            strokeColor="-webkit-linear-gradient(#4f543d, #0ac4c4)"
+            minFontSize={130}
+          />
+        </section>
+        <p>{water.join(" ")}</p>
         {/* <AnimatedComponent /> */}
 
         <button onClick={handleOnClick} className="btn">
